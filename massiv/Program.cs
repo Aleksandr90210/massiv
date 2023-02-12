31773397b54bacdc8bd5c3d73604ae67fddaffb3
Aleksandr90210 <yudin.aleksandrkpc802020@mail.ru>
2023-02-12 21:22:01 +0300

    поменял размеры

--- massiv/Program.cs
+++ massiv/Program.cs
@@ -3,15 +3,15 @@ public class MainClass
 {
     public static void Main()
     {
-        int[,] array1 = new int[2, 3];
-        int[,] array2 = new int[2, 3];
-        int[,] result = new int[2, 3];
+        int[,] array1 = new int[12, 28];
+        int[,] array2 = new int[12, 28];
+        int[,] result = new int[12, 28];
 
         Random random = new Random();
         Console.WriteLine("Первый массив :");
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 12; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < 28; j++)
             {
                 array1[i, j] = random.Next(100);
                 Console.Write("{0,4}", array1[i, j]);
@@ -19,9 +19,9 @@ public class MainClass
             Console.WriteLine();
         }
         Console.WriteLine("Второй массив :");
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 12; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < 28; j++)
             {
                 array2[i, j] = random.Next(100);
                 Console.Write("{0,4}", array2[i, j]);
@@ -29,9 +29,9 @@ public class MainClass
             Console.WriteLine();
         }
         Console.WriteLine("Результат: ");
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 12; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < 28; j++)
             {
                 result[i, j] = array1[i, j] - array2[i, j];
                 Console.Write("{0,4}", result[i, j]);
