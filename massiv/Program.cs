ee41743def514e90308bbe5fef2325a129d67286
Aleksandr90210 <yudin.aleksandrkpc802020@mail.ru>
2023-02-12 21:13:34 +0300

    Добавьте файлы проекта.

---
+++ massiv/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        int[,] array1 = new int[2, 3];
+        int[,] array2 = new int[2, 3];
+        int[,] result = new int[2, 3];
+
+        Random random = new Random();
+        Console.WriteLine("Первый массив :");
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                array1[i, j] = random.Next(100);
+                Console.Write("{0,4}", array1[i, j]);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Второй массив :");
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                array2[i, j] = random.Next(100);
+                Console.Write("{0,4}", array2[i, j]);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Результат: ");
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                result[i, j] = array1[i, j] - array2[i, j];
+                Console.Write("{0,4}", result[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
